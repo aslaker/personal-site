809e5e44356bf1a3949392068745567ca4724916
Adam Slaker <adam.slaker@inspire11.com>
2021-12-06 14:33:37 -0600

    Add data to project page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { query } from ".keystone/api";
+import { NextPage } from "next";
+import type { InferGetStaticPropsType } from "next";
+import { Page, Project } from "../../types/data.types";
 
-const ProjectsPage = () => {
-  return <h1>ProjectsPage works!</h1>;
+const ProjectsPage = ({
+  page,
+  projects,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <div>
+      <h1>{page.headerText}</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <span>{project.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProjectsPage;
+
+export async function getStaticProps() {
+  const page: Page = await query.Page.findOne({
+    where: { name: "Projects" },
+    query: "name headerText",
+  });
+  const projects: Project[] = await query.Project.findMany({
+    query: "id name description",
+  });
+  return {
+    props: { page, projects },
+  };
+}
