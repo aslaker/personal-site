833d0518ade0c4209b0bf6c2ede11bb496e75ba1
Adam Slaker <adam.slaker@inspire11.com>
2021-12-06 10:48:38 -0600

    Add additional home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div className=" flex flex-col p-10 justify-center gap-12 items-end min-h-screen bg-gray-900">
-      <h1 className="text-white font-sans text-3xl">
-        Hi my name is <span className="text-primary-400">Adam Slaker</span>,
-        come see some of the cool stuff I have built
-      </h1>
-      <button className="bg-green-400 rounded-md w-24 h-12 font-bold text-white">
-        Cool Stuff
-      </button>
+    <div className=" flex flex-col p-10 justify-center items-end gap-12 min-h-screen bg-gray-900 text-white">
+      <div className="flex flex-col items-end">
+        <h1 className="font-sans text-2xl">
+          Hi my name is <span className="text-primary-400">Adam Slaker</span>
+        </h1>
+        <p className="text-xl">I like to develop software.</p>
+      </div>
+      <Link href="/projects">
+        <a className="flex justify-center items-center bg-green-400 rounded-md w-24 h-12 font-bold text-white">
+          Enter
+        </a>
+      </Link>
     </div>
   );
 };
