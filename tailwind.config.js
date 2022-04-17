f007c6d10af2d5da9892add7e89ecd752ac3dec3
Adam Slaker <adam.slaker@inspire11.com>
2022-04-17 10:06:54 -0500

    Add TechIcon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx}",
